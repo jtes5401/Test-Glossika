3e2dee8040df53babc34cc334375b2d31eab1452
Wei Kuo <jtes5401@gmail.com>
2021-03-25 12:51:22 +0800

    Add: Take video function

--- Test-Glossika/ViewController.swift
+++ Test-Glossika/ViewController.swift
@@ -6,12 +6,41 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    let pickerViewController = UIImagePickerController()
+    
+    var cameraButton = UIButton(frame: .zero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cameraButton.setTitle("Take video", for: .normal)
+        cameraButton.setTitleColor(.systemBlue, for: .normal)
+        cameraButton.addTarget(self, action: #selector(cameraButtonTouch(sender:)), for: .touchUpInside)
+        
+        self.view.addSubview(cameraButton)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        cameraButton.snp.remakeConstraints { (make) in
+            make.height.width.equalTo(100)
+            make.center.equalToSuperview()
+        }
+    }
+    
+    @objc func cameraButtonTouch(sender:UIButton) {
+        pickerViewController.sourceType = .camera
+        pickerViewController.cameraDevice = .front
+        pickerViewController.mediaTypes = ["public.movie"]
+        pickerViewController.cameraCaptureMode = .video
+        pickerViewController.showsCameraControls = true
+        
+        self.present(pickerViewController, animated: false, completion: nil)
     }
 
 
