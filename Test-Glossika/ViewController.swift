a5c0ec01b4a38873b6911d66764d79bb95db5333
Wei Kuo <jtes5401@gmail.com>
2021-03-25 12:08:25 +0800

    Initial Commit

---
+++ Test-Glossika/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-Glossika
+//
+//  Created by Wei Kuo on 2021/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
